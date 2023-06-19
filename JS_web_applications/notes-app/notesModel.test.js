d735904fd87eef63a3027896f270e4d5d228dba5
Della Izadi <delaram.ae@gmail.com>
2023-06-19 17:15:58 +0100

    Integration Notesmodel into the mail file to make it more accessible on the webpage.

---
+++ JS_web_applications/notes-app/notesModel.test.js
@@ -0,0 +1,28 @@
+const NotesModel = require('./notesModel');
+
+
+it('should create a new NotesModel', () => {
+    const model = new NotesModel();
+    expect(model.getNotes()).toEqual([]);
+  });
+  
+it('should add notes to the NotesModel', () => {
+    const model = new NotesModel();
+  
+    model.addNote('Buy milk');
+    model.addNote('Go to the gym');
+  
+    const notes = model.getNotes();
+    expect(notes).toEqual(['Buy milk', 'Go to the gym']);
+  });
+
+it('should reset the notes in the NotesModel', () => {
+    const model = new NotesModel();
+  
+    model.addNote('Buy milk');
+    model.addNote('Go to the gym');
+    model.reset();
+  
+    const notes = model.getNotes();
+    expect(notes).toEqual([]);
+  });
