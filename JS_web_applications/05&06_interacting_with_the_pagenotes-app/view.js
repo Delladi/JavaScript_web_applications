0915380bd0a9519abddaa6a44e4e68ec2afbc904
Della Izadi <delaram.ae@gmail.com>
2023-06-20 15:33:15 +0100

    chapter 06 exercise 1, Added a passing test for addParagraph for having 3 paragraphs.

---
+++ JS_web_applications/05&06_interacting_with_the_pagenotes-app/view.js
@@ -0,0 +1,14 @@
+class View {
+  constructor() {
+    this.mainContainerEl = document.querySelector('#main-container');
+
+    console.log(this.mainContainerEl);
+  }
+  addParagraph() {
+    const newParagraph = document.createElement('p');
+    newParagraph.innerText = 'I was created dynamically by JavaScript';
+    this.mainContainerEl.append(newParagraph)
+  }
+}
+
+module.exports = View;
---
+++ JS_web_applications/05&06_interacting_with_the_pagenotes-app/view.test.js
@@ -0,0 +1,29 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const fs = require('fs');
+const View = require('./view');
+
+describe('Page view', () => {
+  it('displays 2 paragraphs', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+
+    const view = new View();
+
+    expect(document.querySelectorAll('p').length).toBe(2);
+  });
+
+  it('adds a new paragraph', () =>{
+    //arrange
+    document.body.innerHTML = fs.readFileSync('./index.html');
+    const view = new View();
+
+    //act
+    view.addParagraph();
+    
+    //assert
+    expect(document.querySelectorAll('p').length).toBe(3);
+
+  });
+});
