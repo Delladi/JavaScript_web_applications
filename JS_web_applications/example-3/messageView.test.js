e457e2feca21fc9b0db7d6491906b38e3c9f16d1
Della Izadi <delaram.ae@gmail.com>
2023-06-21 18:02:19 +0100

    Example 3 exercise 2, Added tests for hiding the message that passed after creating the test and adding the button on html file.

--- JS_web_applications/example-3/messageView.test.js
+++ JS_web_applications/example-3/messageView.test.js
@@ -7,13 +7,28 @@ const MessageView = require('./messageView');
 
 describe('MessageView', () => {
   it('clicks the button', () => {
+    //arrange
     document.body.innerHTML = fs.readFileSync('./index.html');
 
     const view = new MessageView();
-
+    //act
     const buttonEl = document.querySelector('#show-message-button');
     buttonEl.click();
-
+    //assert
     expect(document.querySelector('#message')).not.toBeNull();
+  });
+
+  it('Hides the message', () => {
+    //arrange
+    document.body.innerHTML = fs.readFileSync('./index.html');
+
+    const view = new MessageView();
+    //act: show then hide the message
+    const buttonEl = document.querySelector('#show-message-button');
+    buttonEl.click();
+    const hideButtonEl = document.querySelector('#hide-message-button');
+    hideButtonEl.click();
+    //assert: that the msg element is not on the page
+    expect(document.querySelector('#message')).toBeNull();
   });
 });
