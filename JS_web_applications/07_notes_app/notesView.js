c8d5aef7f1bf3505a2d1e9009862205c0e3bef8b
Della Izadi <delaram.ae@gmail.com>
2023-06-22 15:27:48 +0100

    Phase 2. 10 - Added a text box for a note input + defined the addNewNote function.

--- JS_web_applications/07_notes_app/notesView.js
+++ JS_web_applications/07_notes_app/notesView.js
@@ -4,12 +4,30 @@ class NotesView {
     constructor(model) {
       this.model = model;
       this.mainContainerEl = document.querySelector('#main-container');
+
+      document.querySelector('#add-note-btn').addEventListener('click', () => {
+        const newNote = document.querySelector('#add-note-input').value;
+        this.addNewNote(newNote);
+      });
+  
     }
     
+    addNewNote(newNote) {
+      this.model.addNote(newNote);
+      this.displayNotes();
+    }
+
     displayNotes() {
+
+      // Remove all previous notes
+      document.querySelectorAll('.note').forEach(element => {
+          element.remove(); 
+  })
       const notes = this.model.getNotes()
-  
-      // For each note, create and append a new element on the main container
+
+      // Text input reset to an empty value after user clicked button
+      document.querySelector('#add-note-input').value = '';
+
       notes.forEach(note => {
         const noteEl = document.createElement('div');
         noteEl.textContent = note;
